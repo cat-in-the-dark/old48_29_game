f9a667951bb9e06683569f6a068b875a92dc087e
over <over@over-debby>
2014-04-26 12:47:29 +0700

    Game HUD 2

--- core/src/com/catinthedark/hud/GameHud.java
+++ core/src/com/catinthedark/hud/GameHud.java
@@ -7,14 +7,31 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 public class GameHud {
 	private int democracyLevel;
 	private int health;
-	
-	private static final int X_POS = 0;
-	private static final int Y_POS = 0;
+
+	private int X_POS = 0;
+	private int Y_POS = 0;
 
 	private final SpriteBatch spriteBatch = new SpriteBatch();
 	private final ShapeRenderer shapeRenderer = new ShapeRenderer();
+	private final Configurator conf = new Configurator();
+
+	public class Configurator {
+		public Configurator setX(int x) {
+			X_POS = x;
+			return this;
+		}
+
+		public Configurator setY(int y) {
+			Y_POS = y;
+			return this;
+		}
+	}
 
 	public GameHud() {
+	}
+	
+	public Configurator conf(){
+		return conf;
 	}
 
 	public int getDemocracyLevel() {
@@ -32,10 +49,10 @@ public class GameHud {
 	public void setHealth(int health) {
 		this.health = health;
 	}
-	
-	public void draw(){
+
+	public void draw() {
 		shapeRenderer.begin(ShapeType.Line);
-		shapeRenderer.rect(0,0,100,10);
+		shapeRenderer.rect(X_POS, Y_POS, 100, 10);
 		shapeRenderer.end();
 	}
 
--- core/src/com/catinthedark/screens/GameScreen.java
+++ core/src/com/catinthedark/screens/GameScreen.java
@@ -7,19 +7,23 @@ import com.catinthedark.hud.GameHud;
  * Created by Ilya on 26.04.2014.
  */
 public class GameScreen extends Basic2DScreen {
-	
-	final GameHud hud = new GameHud();
-	
-    public GameScreen(BSODGame game, int viewPortWidth, int viewPortHeight) {
-        super(game, viewPortWidth, viewPortHeight);
-        camera.position.set(viewPortWidth / 2f, viewPortHeight / 2f, 0);
-        camera.update();
-    }
-    
-    @Override
-    public void render(float delta) {
-    	super.render(delta);
-    	
-    	hud.draw();
-    }
+
+	final GameHud hud;
+
+	public GameScreen(BSODGame game, int viewPortWidth, int viewPortHeight) {
+		super(game, viewPortWidth, viewPortHeight);
+
+		this.hud = new GameHud();
+		hud.conf().setX(10).setY(570);
+
+		camera.position.set(viewPortWidth / 2f, viewPortHeight / 2f, 0);
+		camera.update();
+	}
+
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+
+		hud.draw();
+	}
 }
