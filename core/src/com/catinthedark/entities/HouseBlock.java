dea01a43d280eca0f6af834e82ba01abf871c892
Kirill <kolbasisha@gmail.com>
2014-04-26 14:19:52 +0700

    Add Enemy, house, houseblock, constants and GameScore.

---
+++ core/src/com/catinthedark/entities/HouseBlock.java
@@ -0,0 +1,43 @@
+package com.catinthedark.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * User: Leyfer Kirill kolbasisha@gmail.com
+ * Date: 26.04.14
+ * Time: 12:55
+ */
+public class HouseBlock extends Entity {
+    private boolean withEnemy = false;
+    private boolean destroyed = false;
+    private int x;
+    private int y;
+    public static final int blockWidth = 2;
+    public static final int blockHeight = 2;
+
+    public boolean isWithEnemy() {
+        return withEnemy;
+    }
+
+    public void setWithEnemy(boolean withEnemy) {
+        this.withEnemy = withEnemy;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public void setDestroyed(boolean destroyed) {
+        this.destroyed = destroyed;
+    }
+
+    public HouseBlock(boolean withEnemy, int x, int y) {
+        super(x, y, blockWidth, blockHeight);
+        setWithEnemy(withEnemy);
+    }
+
+    @Override
+    public void render(float delta, SpriteBatch batch) {
+        super.render(delta, batch);
+    }
+}
