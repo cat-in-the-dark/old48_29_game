ca42afc5668f2e9f4f95e18eeef48113ecb77881
Kirill <kolbasisha@gmail.com>
2014-04-26 17:01:32 +0700

    Free flats for every happy terrorist family!

--- core/src/com/catinthedark/level/LevelGenerator.java
+++ core/src/com/catinthedark/level/LevelGenerator.java
@@ -1,8 +1,15 @@
 package com.catinthedark.level;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.catinthedark.Constants;
+import com.catinthedark.GameScore;
+import com.catinthedark.entities.Entity;
 import com.catinthedark.entities.House;
+import com.catinthedark.screens.GameScreen;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * User: Leyfer Kirill kolbasisha@gmail.com
@@ -11,17 +18,51 @@ import com.catinthedark.entities.House;
  */
 public class LevelGenerator {
     private static LevelGenerator ourInstance = new LevelGenerator();
-    private long baseTime;
+    private float minDistance;
+    private float maxDistance;
 
     public static LevelGenerator getInstance() {
         return ourInstance;
     }
 
     private LevelGenerator() {
-        baseTime = TimeUtils.nanoTime();
     }
 
     public void generateLevel(Level level) {
+        /*
+         minDistance is between 2 * BUILDING_DISTANCE_MIN for minimum level of democracy
+         and BUILDING_DISTANCE_MIN for maximum level of democracy
+         */
+        minDistance = (2f - ((float)GameScore.getInstance().getDemocracyLevel() /
+                Constants.DEMOCRACY_LEVEL_MAX)) * Constants.BUILDING_DISTANCE_MIN;
+        /*
+         maxDistance is between 3 * BUILDING_DISTANCE_MIN for minimum level of democracy
+         and 2 * BUILDING_DISTANCE_MIN for maximum level of democracy
+         */
+        maxDistance = (3f - ((float)GameScore.getInstance().getDemocracyLevel() /
+                Constants.DEMOCRACY_LEVEL_MAX)) * Constants.BUILDING_DISTANCE_MIN;
 
+        Map<Class, ArrayList<Entity>> entities = level.levelEntities;
+        for (Class entityClass : entities.keySet()) {
+            if (entityClass == House.class) {
+                float maxHouseX = 0;
+                float cameraPosition = level.gameScreen.getCamera().position.x;
+                float rightEdgePosition = cameraPosition + level.gameScreen.getCamera().viewportWidth / 2f;
+
+                for (Entity house : entities.get(entityClass)) {
+                    if (house.getX() > maxHouseX) {
+                        maxHouseX = house.getX();
+                    }
+                }
+
+                if (rightEdgePosition - maxHouseX > minDistance) {
+                    float newHouseX = rightEdgePosition + MathUtils.random(minDistance, maxDistance);
+                    int heightInBlocks = MathUtils.random(2, 4);
+                    int widthInBlocks = MathUtils.random(2, 3);
+                    Entity houseEntity = new House(newHouseX, Constants.GROUND_LEVEL, widthInBlocks, heightInBlocks);
+                    level.levelEntities.get(entityClass).add(houseEntity);
+                }
+            }
+        }
     }
 }
